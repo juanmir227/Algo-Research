5f54ec08f96c551d9c12f2c7af25d44edb416084
juan <juanmir227@gmail.com>
2022-10-02 19:38:34 -0300

    yes

--- functions/open_pickle.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("new_block_data/block_data.pickle", 'rb') as what:
-    bloques = pickle.load(what)
-
-print(bloques[0]["block"])
--- functions/prueba.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-print(type(os.environ["PURESTAKE_API"]))
-# usuario = env.prefix('path')
-# usuario
